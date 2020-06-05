3a631070e0f369e553a6fe5625564380cbbe55a7
Peng-Wen Lai <lailai5203@gmail.com>
2020-06-05 16:22:24 -0700

    changes to js

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ const projects = [
 const list = document.getElementById('list');
 
 projects.forEach(project => {
+    const listItem = document.createElement('li');
     const link = document.createElement('a');
     link.href = `/${project}/index.html`;
-    link.innerText = project;
+    link.innerText = formatProjectName(project);
 
-    list.appendChild(link);
+    const img = document.createElement('img');
+    img.src = `/${project}/design/desktop-design.jpg`;
+
+    listItem.appendChild(img);
+    listItem.appendChild(link);
+    list.appendChild(listItem);
 
 });
+
+function formatProjectName(name) {
+    return name.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
